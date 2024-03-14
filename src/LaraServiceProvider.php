1e92bea5d5c188e35c0b99c90248e61f9824513d
Ganesh Kandu ( गणेश कांदु ) <kanduganesh@gmail.com>
2024-03-14 16:22:15 +0530

    Update LaraServiceProvider.php

--- src/LaraServiceProvider.php
+++ src/LaraServiceProvider.php
@@ -6,5 +6,50 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class LaraServiceProvider extends ServiceProvider{
-	
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->registerRoutes();
+		$this->registerResources();
+	}
+
+	/**
+     * Register the lara resources.
+     *
+     * @return void
+     */
+    protected function registerResources()
+    {
+        $this->loadViewsFrom([__DIR__.'/../resources/views'], 'lara');
+    }
+
+	/**
+	 * Register the package routes.
+	 *
+	 * @return void
+	 */
+	protected function registerRoutes()
+	{
+		Route::group([
+            'namespace' => 'ganeshkandu/lara',
+			'prefix' => config('lara.path'),
+        ], function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        });
+		
+	}
 }
